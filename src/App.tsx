0d0a11cc47dd22507e0ec757387fb1b23a67e8be
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2021-09-26 20:11:26 -0300

    Configure mirageJS to fake an API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { createServer } from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions',(() => {
+      return [
+          {
+            id: 1,
+            title: 'WebSite Freela',
+            amount: 400,
+            type: 'deposit',
+            category: 'Development',
+            createdAt: new Date()
+          }
+        ]
+      })
+    )
+  }
+})
 
 export function App() {
   return (
